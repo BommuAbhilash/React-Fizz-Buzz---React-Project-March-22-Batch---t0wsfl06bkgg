c7c9a448d147b9afbb9b462b076bc3afddd0e1b5
BommuAbhilash <bommuabhi.ba@gmail.com>
2022-10-01 15:19:54 +0530

    2nd commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css';
+
 const App = () => {
+  const [count,setCount] = useState(1);
+  const isEnabled = false;
+
+  const handleIncrement = () => {
+    setCount(count+1);
+  }
+  
+  const handleDecrement = () => {
+    setCount(count-1);
+  }
 
   
+
   return (
     <div id="main">
-
+      <button  onClick={handleIncrement}>Increment</button>
+      <p className={(count%3==0 && count%5==0) ? "fizzbuzz" : (count%3==0) ? "fizz" : (count%5==0) ? "buzz" : "normal"}>{count}</p>
+      <button onClick={handleDecrement}>Decrement</button>
     </div>
   )
 }
 
 
 export default App;
+// 
